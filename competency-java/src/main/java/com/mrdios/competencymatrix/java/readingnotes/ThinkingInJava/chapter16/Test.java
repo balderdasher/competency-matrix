4a04625bf9f5d794d095af2bd67d57bd87036b91
huxiong <huxiong@news.cn>
2016-07-12 17:08:00 +0800

    new files for chapter17-容器深入

---
+++ competency-java/src/main/java/com/mrdios/competencymatrix/java/readingnotes/ThinkingInJava/chapter16/Test.java
@@ -0,0 +1,13 @@
+package com.mrdios.competencymatrix.java.readingnotes.ThinkingInJava.chapter16;
+
+/**
+ * @author huxiong
+ * @date 2016/07/12 11:29
+ */
+public class Test {
+    char c;
+    public static void main(String[] args) {
+        Test t = new Test();
+        System.out.println("---"+t.c);
+    }
+}
