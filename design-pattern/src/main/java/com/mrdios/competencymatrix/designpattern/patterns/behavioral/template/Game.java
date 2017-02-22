eb620c45070f269233dc0eba70a5f114803d95cb
mrdios <balderdasher@msn.com>
2017-02-22 16:25:46 +0800

    change

--- design-pattern/src/main/java/com/mrdios/competencymatrix/designpattern/patterns/behavioral/template/Game.java
+++ design-pattern/src/main/java/com/mrdios/competencymatrix/designpattern/patterns/behavioral/template/Game.java
@@ -16,7 +16,7 @@ public abstract class Game {
     /**
      * 模板方法，用于确定解决一个问题的顺序，不允许子类重写，子类可以重写模板方法中所调用的具体步骤方法
      */
-    public final void play() {
+    final void play() {
         init();
         startPlay();
         endPlay();
